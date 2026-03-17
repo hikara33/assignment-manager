bf80725ff8fc77c5ff6b50bd5352b29f0179e3c8
Eva Antonushkina <eant367@mail.ru>
2026-03-17 20:53:28 +0300

    test: add db rollback

---
+++ test/utils/db.ts
@@ -0,0 +1,10 @@
+import { PrismaService } from "src/prisma/prisma.service";
+
+export async function startTestTx(prisma: PrismaService) {
+  await prisma.$executeRawUnsafe('BEGIN');
+  return prisma;
+}
+
+export async function rollbackTestTx(prisma: PrismaService) {
+  await prisma.$executeRawUnsafe('ROLLBACK');
+}
