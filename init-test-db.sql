35894d860cbbfd7b1f536dc31f43b253d9422482
Eva Antonushkina <eant367@mail.ru>
2026-04-03 23:56:12 +0300

    fix: docker-compose.test

--- init-test-db.sql
+++ init-test-db.sql
@@ -1,1 +1,9 @@
-CREATE DATABASE assignment_test;
+-- init-test-db.sql
+DO
+$body$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'assignment_test') THEN
+      CREATE DATABASE assignment_test;
+   END IF;
+END
+$body$;
